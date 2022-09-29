5b9bc78982cde0a7385b3703442fb8d27f1a6cc7
lolollllo <99907738+lolollllo@users.noreply.github.com>
2022-09-29 22:56:47 +0500

    fix

--- src/types/MessageData.ts
+++ src/types/MessageData.ts
@@ -1,11 +1,16 @@
-import { EmbedBuilder, FileOptions } from 'discord.js';
+import { EmbedBuilder } from 'discord.js';
+import { Buffer } from 'node:buffer';
 
 export interface MessageData {
     username: string;
     avatar?: string;
     content?: string;
-    embeds?: EmbedBuilder[];
-    files?: FileOptions[];
+    embeds?: EmbedBuilder;
+    files?: {
+        name: string;
+        description: string;
+        attachment: string | Buffer;
+    };
     pinned?: boolean;
     sentAt: string;
 }
